3e2b5c31801a4e3bba1ffc8a413ecc1ac5a9a491
Taehwan Kim <maxtortime@gmail.com>
2018-11-27 22:53:34 +0900

    Updates

---
+++ precache-manifest.491fe12d1206b9ce25e94b05bea5decc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01bab01ed180ddc321a2",
+    "url": "/ddocdoc-test/static/css/main.fb14f5a2.chunk.css"
+  },
+  {
+    "revision": "01bab01ed180ddc321a2",
+    "url": "/ddocdoc-test/static/js/main.01bab01e.chunk.js"
+  },
+  {
+    "revision": "7e61a862b46c37f7ee15",
+    "url": "/ddocdoc-test/static/js/1.7e61a862.chunk.js"
+  },
+  {
+    "revision": "719f132de305992a6edd",
+    "url": "/ddocdoc-test/static/js/runtime~main.719f132d.js"
+  },
+  {
+    "revision": "4b837a21280abf0920522bc6aad4fbc3",
+    "url": "/ddocdoc-test/static/media/sprite.4b837a21.png"
+  },
+  {
+    "revision": "9d10acbbe88f27bf6c91ae325a83bee1",
+    "url": "/ddocdoc-test/index.html"
+  }
+];
