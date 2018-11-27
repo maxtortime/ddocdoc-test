b6f0d4aab826a8b64b19cc523818ec64ed97b3ed
Taehwan Kim <maxtortime@gmail.com>
2018-11-27 13:31:11 +0900

    Add new icon and point

--- src/MenuHeaderItem.js
+++ src/MenuHeaderItem.js
@@ -3,16 +3,22 @@ import React from 'react';
 export default (props) => {
     let className = 'menu-header-item';
     let newIcon = '';
+    let anchor = (<a href={props.menuHref}>{props.menuText}</a>);
 
     if (props.menuText === '기획전') {
         className +=' menu-header-active-item'
+        newIcon = (
+            <div className="icon icon-curation-new-badge new-items-icon"></div>
+        )
+
+        anchor = (<a href={props.menuHref}><span className="red-point">{props.menuText}</span></a>);
     }
 
     return (
         <li className={className}>
             <div>
                 {newIcon}
-                <a href={props.menuHref}>{props.menuText}</a>
+                {anchor}
             </div>
         </li>
     )
